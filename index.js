11d4e8c11b668add10bf328c346551d38f383980
teddddd <ted@mixpanel.com>
2016-01-23 19:38:25 +0000

    run source through virtual-jade and add export

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var vjade = require('virtual-jade');
+
 module.exports = function(source) {
-  return source;
+  return vjade(source, {name: 'template'}) +
+    "\nmodule.exports = function(locals) { template(locals || {}); };";
 };
--- test/loader.test.js
+++ test/loader.test.js
@@ -2,7 +2,7 @@ var expect = require('expect.js');
 var loader = require('../');
 
 describe('loader', function() {
-  it('temporarily returns identity', function() {
-    expect(loader('.bla Hello world')).to.eql('.bla Hello world');
+  it('creates a template function', function() {
+    expect(loader('.bla Hello world')).to.contain('function template(');
   });
 });
