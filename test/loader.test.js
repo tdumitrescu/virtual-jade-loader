ce5f31b8def60cc73b4c0ecba65fdf4b06b35e94
teddddd <ted@mixpanel.com>
2016-01-23 19:46:28 +0000

    basic tests

--- test/loader.test.js
+++ test/loader.test.js
@@ -2,7 +2,21 @@ var expect = require('expect.js');
 var loader = require('../');
 
 describe('loader', function() {
+  var loaded = loader('.bla Hello world');
+
   it('creates a template function', function() {
-    expect(loader('.bla Hello world')).to.contain('function template(');
+    expect(loaded).to.contain('function template(');
+  });
+
+  it('exports a template function', function() {
+    expect(loaded).to.contain('exports = function(locals)');
+  });
+
+  it('returns a virtual-dom node from the template function', function() {
+    expect(loaded).to.contain('h("div", {');
+  });
+
+  it('passes static text content', function() {
+    expect(loaded).to.contain('Hello world');
   });
 });
