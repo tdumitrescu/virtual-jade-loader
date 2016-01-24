983e5b3c90d0b07bbb7eb3ff554da2c074f5170f
teddddd <ted@mixpanel.com>
2016-01-24 04:03:41 +0000

    explicit return plz

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var vjade = require('virtual-jade');
 module.exports = function(source) {
   this.cacheable && this.cacheable();
   return vjade(source, {name: 'template'}) +
-    "\nmodule.exports = function(locals) { template(locals || {}); };";
+    "\nmodule.exports = function(locals) { return template(locals || {}); };";
 };
