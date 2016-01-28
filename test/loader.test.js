143ec76987827d60a37ba177e239496a624aa8a8
teddddd <ted@mixpanel.com>
2016-01-28 16:21:30 +0000

    fix tests

--- test/loader.test.js
+++ test/loader.test.js
@@ -5,11 +5,11 @@ describe('loader', function() {
   var loaded = loader('.bla Hello world');
 
   it('creates a template function', function() {
-    expect(loaded).to.contain('function template(');
+    expect(loaded).to.contain('function _jade_template_fn(');
   });
 
   it('exports a template function', function() {
-    expect(loaded).to.contain('exports = function(locals)');
+    expect(loaded).to.contain('exports = _jade_template_fn');
   });
 
   it('returns a virtual-dom node from the template function', function() {
